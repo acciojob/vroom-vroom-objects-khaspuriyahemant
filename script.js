e04032a0660853774c68883c64dce2bb9862ce36
khaspuriyahemant <104751396+khaspuriyahemant@users.noreply.github.com>
2026-01-01 01:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+car.prototype.getMakeModel=function(){
+	return '${this.make} ${this.model}';
+};
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed=topSpeed;
+}
+
+SportCar.prototype=Object.create(car.prototype);
+SportCar.prototype.constructor=SportsCar;
+
+SportCar.prototype.getTopSpeed=function(){
+	return this.topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
