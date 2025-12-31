cbc6c7be2ccbacf921cdd60617f9e414505ce433
khaspuriyahemant <104751396+khaspuriyahemant@users.noreply.github.com>
2026-01-01 01:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-// Complete the js code
+// Step 1: Car Constructor
 function Car(make, model) {
-	this.make=make;
-	this.model=model;
+    this.make = make;
+    this.model = model;
 }
-car.prototype.getMakeModel=function(){
-	return '${this.make} ${this.model}';
+
+// Step 2: Add getMakeModel Method
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
 };
 
+// Step 3: SportsCar Constructor
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model);
-	this.topSpeed=topSpeed;
+    Car.call(this, make, model); // Call the Car constructor
+    this.topSpeed = topSpeed;
 }
 
-SportsCarCar.prototype=Object.create(car.prototype);
-SportsCarCar.prototype.constructor=SportsCar;
+// Step 4: Add getTopSpeed Method
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
 
-SportsCar.prototype.getTopSpeed=function(){
-	return this.topSpeed;
-}
+// Step 5: Set Up Inheritance
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+// Example Usage
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
